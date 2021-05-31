dd664e22031786f859d216f4cfcd5d4b7934fa34
Fahima 42 <Fahima42@MacBook-Air-de-Fahima.local>
2021-05-31 13:44:19 +0200

    a

---
+++ ft_substr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/31 08:03:19 by marvin            #+#    #+#             */
+/*   Updated: 2021/05/31 09:04:04 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char *new;
+    char *src;
+    unsigned int i;
+    unsigned int j;
+    unsigned int length;
+    
+    src = (char *)s;
+    i = 0;
+    j = 0;
+    length = ft_strlen(src);
+    new = (char *)malloc(sizeof(char) * (len + 1));
+    if (!new)
+        return (NULL);
+    if (start > length)
+        return (NULL);
+    while (src[i] && i < length && j < len)
+    {
+        while (i != start)
+            i++;
+        if (i == start)
+            new[j] = src[i];
+        i++;
+        start++;
+        j++;
+    }
+    new[j] = '\0';
+    return (new);
+}   
