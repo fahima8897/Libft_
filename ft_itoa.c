c3e000ceb8f6e0545a2695047ad49e2fdb66ebc0
Fahima 42 <Fahima42@MacBook-Air-de-Fahima.local>
2021-06-14 22:42:57 +0200

    a

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,17 +6,50 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 16:50:53 by marvin            #+#    #+#             */
-/*   Updated: 2021/06/04 10:44:45 by fboumell         ###   ########.fr       */
+/*   Updated: 2021/06/14 20:41:16 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int i)
+static int ft_count(int n)
 {
-	char	*new;
+    int size;
 
-	new = malloc(sizeof(char) *);
-	if (!new)
-		return (NULL);
+    if(n <= 0)
+        size = 1;
+    else
+        size = 0;
+    while (n != 0)
+    {
+        n = n / 10;
+        size++;
+    }
+    return (size);
+}
+ char    *ft_itoa(int n)
+{
+    char    *new;
+    int     len;
+    int long    c;
+    
+    c = n;
+    if (c < 0)
+        c = -c;
+    len = ft_count(n);
+    new = malloc(sizeof(char) * (len + 1));
+    if (!new)
+        return (NULL);
+    new[len] = '\0';
+    if(c == 0)
+        new[0] = '0';
+    while ((len - 1) >= 0)
+    {
+        new[len - 1] = c % 10 + '0';
+        c = c / 10;
+        len --;
+    }
+    if (n < 0)
+        new[0] = '-';
+    return (new);
 }
